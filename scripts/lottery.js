b4624884cf0345350f6c5c229e7a6feedcf44899
haze111nuts <hasami75@gmail.com>
2024-05-13 01:44:16 -0400

    add some basic set up

--- scripts/lottery.js
+++ scripts/lottery.js
@@ -1,5 +1,23 @@
+
+var ocs = [{},{},{},{},{},{}];
+
+function getOcUrl(id) {
+    return "assets/lottery/2024profile/" + id + ".jpg";
+}
+
+function printOCs() {
+    var ocHtml = "";
+    for (var i = 0; i < ocs.length; i++) {
+        ocHtml += "<div>";
+        ocHtml += "<img src='"+ getOcUrl(i) +"'>";
+        ocHtml += "</div>";
+    }
+    $(".turingBar").html(ocHtml);
+    console.log(ocHtml);
+}
+
 function setupStuff() {
-    console.log("test");
+    printOCs();
 }
 
 //======================//
