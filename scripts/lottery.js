277c80d6e2435d6c52aa576eb1790e0bf10f3084
haze111nuts <hasami75@gmail.com>
2024-05-15 03:41:44 -0400

    styling lottery page

--- scripts/lottery.js
+++ scripts/lottery.js
@@ -1,5 +1,10 @@
 
-var ocs = [{},{},{},{},{},{}];
+var OCS = [{},{},{},{},{},{},{},{}];
+
+var GIFT_SLOTS = [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}];
+
+
+var NUMBER_OF_BG = 75
 
 function getOcUrl(id) {
     return "assets/lottery/2024profile/" + id + ".jpg";
@@ -7,17 +12,51 @@ function getOcUrl(id) {
 
 function printOCs() {
     var ocHtml = "";
-    for (var i = 0; i < ocs.length; i++) {
+    for (var i = 0; i < OCS.length; i++) {
         ocHtml += "<div>";
         ocHtml += "<img src='"+ getOcUrl(i) +"'>";
         ocHtml += "</div>";
     }
     $(".turingBar").html(ocHtml);
-    console.log(ocHtml);
+
+    //transform: translate(-50px, 0);
+    $(".turingBar").css("transform", "translate(-650px, 0)");
+}
+
+function printGrid(){
+    var gridHtml = "";
+    for (var i = 0; i < GIFT_SLOTS.length; i++) {
+        gridHtml += "<div class='gridItem'>";
+        gridHtml += "<div class='gridItem_inner'>";
+
+        gridHtml += "<div class='gift_front'>";
+        gridHtml += i+1;
+        gridHtml += "</div>";
+
+        gridHtml += "<div class='gift_back'>";
+        gridHtml += "";
+        gridHtml += "</div>";
+
+        gridHtml += "</div>";
+        gridHtml += "</div>";
+    }
+    $(".grid").html(gridHtml);
+}
+
+function setGridBG(){
+    for (var i = 0; i < GIFT_SLOTS.length; i++) {
+        $(".grid .gridItem:nth-of-type("+ (i+1) +") .gift_front").css("background-image","url(assets/lottery/bg/"+ randomBGIndex() +".png)");
+    }
+}
+
+function randomBGIndex(){
+    return Math.floor(Math.random() * NUMBER_OF_BG) +1
 }
 
 function setupStuff() {
     printOCs();
+    printGrid();
+    setGridBG();
 }
 
 //======================//
