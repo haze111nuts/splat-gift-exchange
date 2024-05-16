6ff6953cd602a8cd48386ea3b9069d7225c3eaa8
haze111nuts <hasami75@gmail.com>
2024-05-16 02:03:32 -0400

    add style for focused OC

--- scripts/lottery.js
+++ scripts/lottery.js
@@ -1,7 +1,10 @@
 
-var OCS = [{},{},{},{},{},{},{},{}];
+var OCS = [
+    {name:"成步堂龍一"}, {name:"王泥喜法介"}, {name:"希月心音"}, 
+    {name:"御劍伶侍"}, {name:"綾里真宵"}, {name:"成步堂美貫"}, 
+    {name:"寶月茜"}, {name:"綾里春美"}];
 
-var GIFT_SLOTS = [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}];
+var GIFT_SLOTS = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
 
 
 var NUMBER_OF_BG = 75;
@@ -18,32 +21,59 @@ function printOCs() {
     var ocHtml = "";
     for (var i = 0; i < OCS.length; i++) {
         ocHtml += "<div class='oc'>";
-        ocHtml += "<img src='"+ getOcUrl(i) +"'>";
+        ocHtml += "<img src='" + getOcUrl(i) + "'>";
         ocHtml += "</div>";
     }
     $(".turingBar").html(ocHtml);
 
     setSpotlightToOC();
+    //animateArrow();
+    animateFrame();
+}
+
+function animateArrow() {
+    const $arrow = document.querySelector('.arrow');
+    $arrow.animate([
+        { left: '10px' },
+        { left: '0' },
+        { left: '10px' }
+    ], {
+        duration: 500,
+        iterations: Infinity
+    });
+}
+
+function animateFrame() {
+    const $arrow = document.querySelector('.frame');
+    $arrow.animate([
+        { transform: 'translate(-50%, 0) scale(1)' },
+        { transform: 'translate(-50%, 0) scale(1.2)' },
+        { transform: 'translate(-50%, 0) scale(1)' }
+    ], {
+        duration: 500,
+        iterations: Infinity
+    });
 }
 
 function getCurrentOCPos() {
     return ORIGINAL_OC_POS + ((CURRENT_OC_INDEX) * -100);
 }
 
-function setSpotlightToOC(){
-    console.log("moving to "+ CURRENT_OC_INDEX + "th OC");
-    console.log("Pos is "+getCurrentOCPos())
-    $(".turingBar").css("transform", "translate("+getCurrentOCPos()+"px, 0)");
+function setSpotlightToOC() {
+    console.log("moving to " + CURRENT_OC_INDEX + "th OC");
+    console.log("Pos is " + getCurrentOCPos())
+    $(".turingBar").css("transform", "translate(" + getCurrentOCPos() + "px, 0)");
+    $(".currentName").html(OCS[CURRENT_OC_INDEX].name);
 }
 
-function printGrid(){
+function printGrid() {
     var gridHtml = "";
     for (var i = 0; i < GIFT_SLOTS.length; i++) {
         gridHtml += "<div class='gridItem'>";
         gridHtml += "<div class='gridItem_inner'>";
 
         gridHtml += "<div class='gift_front'>";
-        gridHtml += i+1;
+        gridHtml += i + 1;
         gridHtml += "</div>";
 
         gridHtml += "<div class='gift_back'>";
@@ -56,35 +86,35 @@ function printGrid(){
     $(".grid").html(gridHtml);
 }
 
-function setGridBG(){
+function setGridBG() {
     for (var i = 0; i < GIFT_SLOTS.length; i++) {
-        $(".grid .gridItem:nth-of-type("+ (i+1) +") .gift_front").css("background-image","url(assets/lottery/bg/"+ randomBGIndex() +".png)");
+        $(".grid .gridItem:nth-of-type(" + (i + 1) + ") .gift_front").css("background-image", "url(assets/lottery/bg/" + randomBGIndex() + ".png)");
     }
 }
 
-function setUpFlipEvent(){
+function setUpFlipEvent() {
     $(".gridItem_inner").each(function () {
         $(this).find(".gift_front").click(function () {
-            $(this).parent(".gridItem_inner").css("transform","rotateY(180deg)");
-            $(this).parent(".gridItem_inner").css("border","rgba(92, 83, 73, 0.308) 1px solid");
+            $(this).parent(".gridItem_inner").css("transform", "rotateY(180deg)");
+            $(this).parent(".gridItem_inner").css("border", "rgba(92, 83, 73, 0.308) 1px solid");
             CURRENT_OC_INDEX++;
             setSpotlightToOC();
             // $(".oc:nth-child("+CURRENT_OC_INDEX+")").css("filter","grayscale(1)");
-            $(".oc:nth-child("+CURRENT_OC_INDEX+")").css("opacity",0.3);
-            $(".oc:nth-child("+(CURRENT_OC_INDEX-2)+")").css("opacity",0.2);
-            $(".oc:nth-child("+(CURRENT_OC_INDEX-4)+")").css("opacity",0);
+            setUpOCStyle();
         })
     })
 }
 
-// function setUpOCStyle(){
-//     $(".oc").each(function () {
-//         $(this).css("filter","grayscale(1)");
-//     })
-// }
+function setUpOCStyle() {
+    $(".oc:nth-child(" + (CURRENT_OC_INDEX + 1) + ")").css("opacity", 1);
+    $(".oc:nth-child(" + CURRENT_OC_INDEX + ")").css("opacity", 0.20);
+    $(".oc:nth-child(" + (CURRENT_OC_INDEX - 1) + ")").css("opacity", 0.10);
+    $(".oc:nth-child(" + (CURRENT_OC_INDEX - 2) + ")").css("opacity", 0.05);
+    $(".oc:nth-child(" + (CURRENT_OC_INDEX - 3) + ")").css("opacity", 0);
+}
 
-function randomBGIndex(){
-    return Math.floor(Math.random() * NUMBER_OF_BG) +1
+function randomBGIndex() {
+    return Math.floor(Math.random() * NUMBER_OF_BG) + 1
 }
 
 function setupStuff() {
