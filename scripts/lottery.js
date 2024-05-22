84c4d63f9b976065ccd5415b4d20f9bb87c11b7c
haze111nuts <hasami75@gmail.com>
2024-05-22 19:30:00 -0400

    forgot to save

--- scripts/lottery.js
+++ scripts/lottery.js
@@ -211,7 +211,7 @@ function getGiftLogHtml(currentOC, gift) {
 
 function printSnow() {
     var snowHtml = "";
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 120; i++) {
         snowHtml += "<div class='snow'></div>";
     }    
     $(".snowScreen").html(snowHtml);
