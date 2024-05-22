dcb6eb35aa78e4661f23d4067e39200176c94a16
chsiao58 <chsiao58@gmail.com>
2024-05-22 00:02:16 -0400

    description for gift

--- scripts/lottery.js
+++ scripts/lottery.js
@@ -8,6 +8,7 @@ const OCS = [
         profilePic: "0.jpg",
         giftName: "葡萄汁",
         giftPic: "0.png",
+        giftDescription: "用特產巨峰葡萄做的100%無濃縮果汁，裝在漂亮的玻璃瓶裡面。當然不含酒精。",
         artist: "1"
     },
     {
@@ -15,6 +16,7 @@ const OCS = [
         profilePic: "1.jpg",
         giftName: "折疊式天文望遠鏡",
         giftPic: "1.png",
+        giftDescription: "口徑7公分、長為26公分的迷你形望遠鏡。有20mm和10mm的數位轉接目鏡、手機攝影轉接架﹐還附有簡易天文觀測指南。除了拿來觀星賞月以外﹐似乎也很適合拿來賞鳥。",
         artist: "4"
     },
     {
@@ -22,6 +24,7 @@ const OCS = [
         profilePic: "2.jpg",
         giftName: "碰可玩具機器人",
         giftPic: "2.png",
+        giftDescription: "大河原宇宙中心的最新紀念品﹐1/8比例的多功能碰可玩具機器人，有著跟原物一樣人工智能與，可以透過聲控來做為日常的小幫手。附一個機器人充電台。\n\n功能：\n- 鬧鐘、備忘錄、上網查詢資料等通知等日常實用機能。\n- 帶去大河原宇宙中心的話，可以當作迷你導覽。\n- 為了人類發展的未來，偶爾會推薦你捐錢給大河原宇宙中心。",
         artist: "5"
     },
     {
@@ -29,6 +32,7 @@ const OCS = [
         profilePic: "3.jpg",
         giftName: "西洋棋組",
         giftPic: "3.png",
+        giftDescription: "特別訂做的紅白色西洋棋組。以桃花心木製成，並鑲有金邊。",
         artist: "1"
     },
     {
@@ -36,6 +40,7 @@ const OCS = [
         profilePic: "4.jpg",
         giftName: "將軍超人第一季DVD全套",
         giftPic: "4.png",
+        giftDescription: "初代將軍超人的DVD影集﹐",
         artist: "1"
     },
     {
@@ -43,6 +48,7 @@ const OCS = [
         profilePic: "5.jpg",
         giftName: "帽子先生吊飾",
         giftPic: "5.png",
+        giftDescription: "手工做的帽子先生吊飾﹐還附有三張驚奇酒吧的免費入場券。",
         artist: "4"
     },
     {
@@ -50,6 +56,7 @@ const OCS = [
         profilePic: "6.jpg",
         giftName: "冬季限定花林糖",
         giftPic: "6.png",
+        giftDescription: "使用北海道小麥的麵粉製成的花林糖﹐是冬季限定的牛奶口味。拿來丟人一定很浪費。",
         artist: "4"
     },
     {
@@ -57,6 +64,7 @@ const OCS = [
         profilePic: "7.jpg",
         giftName: "倉院特產饅頭",
         giftPic: "7.png",
+        giftDescription: "倉院之里的特產栗子饅頭。每個饅頭都是勾玉形狀﹐有經過某位靈媒的祈禱加持﹐據說吃下去可以補充靈力。",
         artist: "2"
     },
     {
@@ -64,6 +72,7 @@ const OCS = [
         profilePic: "8.jpg",
         giftName: "手工押花",
         giftPic: "8.png",
+        giftDescription: "蒼苑國特有的花製成的書籤﹐還有淡淡的花香。",
         artist: "6"
     }
 ];
