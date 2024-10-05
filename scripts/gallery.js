8426274488292413829932d704bbdbb65ebb4f92
haze111nuts <hasami75@gmail.com>
2024-10-05 18:15:03 -0400

    finalize gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -30,9 +30,41 @@ function getGiftUrl(id) {
     return "assets/" + YEAR + "/item/" + id + ".png";
 }
 
-//=======================//
-//=== HTML Generators ===//
-//=======================//
+
+//=============================//
+//=== Handle Exchange Click ===//
+//=============================//
+
+function setUpFlipToggle() {
+    $(".exchangeButton").click(function () {
+        if (displayGroup === "sender") {
+            //set to getter
+            $(".cardInner").css("transform", "rotateY(180deg)");
+            displayGroup = "getter";
+            $("body").addClass("night");
+            $("body").removeClass("day");
+
+            $(".exchangeIcon").css("left", "200px");
+            $(".exchangeBar_active").css("width", "284px");
+            $(".exchangeIcon img").attr('src', "assets/changeIcon_open.png" );
+            
+        } else {
+            //set to sender
+            $(".cardInner").css("transform", "rotateY(0deg)");
+            displayGroup = "sender";
+            $("body").addClass("day");
+            $("body").removeClass("night");
+
+            $(".exchangeIcon").css("left", "-80px");
+            $(".exchangeBar_active").css("width", "0px");
+            $(".exchangeIcon img").attr('src', "assets/changeIcon.png" );
+        }
+    });
+}
+
+//======================//
+//=== Settingup Grid ===//
+//======================//
 
 function generateGrid() {
     var gridHtml = "";
@@ -80,48 +112,13 @@ function setUpGridStyle(){
         // delay style for each grid tem
         $(".grid-item:nth-child(" + (i + 1) + ") .cardInner")
         .css("transition-delay", i * 0.03 + "s");
-
-        // $(".grid-item:nth-child(" + (i + 1) + ") .cardBG")
-        // .css("background-color",ART_COLOR[i]);
     }
 }
 
-function setUpFlipToggle() {
-    $(".exchangeButton").click(function () {
-        if (displayGroup === "sender") {
-            //set to getter
-            $(".cardInner").css("transform", "rotateY(180deg)");
-            displayGroup = "getter";
-            $("body").addClass("night");
-            $("body").removeClass("day");
 
-            $(".exchangeIcon").css("left", "200px");
-            $(".exchangeBar_active").css("width", "284px");
-            $(".exchangeIcon img").attr('src', "assets/changeIcon_open.png" );
-
-            // $(".grid-item").css("background-color", "#fff");
-            // $(".cardBG").css("transform", "scale(1.05) rotate(-93deg)");
-            // $(".cardBG").css("background-color", "#94cbdb");
-            
-        } else {
-            //set to sender
-            $(".cardInner").css("transform", "rotateY(0deg)");
-            displayGroup = "sender";
-            $("body").addClass("day");
-            $("body").removeClass("night");
-
-            $(".exchangeIcon").css("left", "-80px");
-            $(".exchangeBar_active").css("width", "0px");
-            $(".exchangeIcon img").attr('src', "assets/changeIcon.png" );
-
-            // $(".grid-item").css("background-color", "#b38242");
-            // $(".cardBG").css("transform", "scale(1.05) rotate(3deg)");
-            // $(".cardBG").css("background-color", "#b37c3f");
-            
-        }
-    });
-}
-
+//============================//
+//=== Settingup Item Modal ===//
+//============================//
 
 function setUpItemModalClickEvents(){
     $(".label").each(function () {
@@ -129,7 +126,7 @@ function setUpItemModalClickEvents(){
             var dataID = $(this).data().id;
             $(".modal").removeClass("hide");
             var itemModalHtml = "";
-
+            itemModalHtml += "<div class='close'>"+"</div>";
             itemModalHtml += "<div class='itemPanel'>";
             itemModalHtml += "<div class='itemArtWrap'>";
             itemModalHtml += "<img class='itemArt' src='" + getGiftUrl(dataID) + "' alt='item' draggable='false' >";
@@ -142,6 +139,7 @@ function setUpItemModalClickEvents(){
             }
             itemModalHtml += "</div>";
             itemModalHtml += "<div class='itemSummary'>";
+            itemModalHtml += "<div class='langSwitch'>"+"⇆"+"</div>";
             itemModalHtml += "<div class='itemTitle'>";
             itemModalHtml += "<div class='itemTitle1'>"+ENTRIES[dataID].giftName+"</div>";
             itemModalHtml += "<div class='itemTitle2'>"+ENTRIES[dataID].giftNameAlt+"</div>";
@@ -162,6 +160,9 @@ function setUpItemModalClickEvents(){
 
 function setUpGiftAltArt(entry,id) {
     handleAltArtIndicator();
+    if(entry["numOfAlt"] != undefined){
+        $(".itemArt").css("cursor","pointer");
+    }
     $(".itemArt").click(function () {
         if(entry["numOfAlt"] != undefined){
             CURRENT_ALT_INDEX = (CURRENT_ALT_INDEX < entry.numOfAlt )? CURRENT_ALT_INDEX+1 : 0;
@@ -180,6 +181,9 @@ function handleAltArtIndicator(){
     $(".itemArtList span").not(':eq(' + CURRENT_ALT_INDEX + ')').css('color', "rgba(82, 68, 61, 0.4)" );
 }
 
+//===========================//
+//=== Settingup Art Modal ===//
+//===========================//
 
 function setUpArtModalClickEvents() {
     $(".cardFront").each(function () {
@@ -191,7 +195,9 @@ function setUpArtModalClickEvents() {
             modalHtml += "<div class='art_wrap'>"
             modalHtml += "<img class='art' src='" + getArtUrl(dataID, "sender") + "'>"
             modalHtml += "<div class='author'>";
-            modalHtml += ENTRIES[dataID].ocName+"<br>By <a aref='"+ ENTRIES[dataID].artist+" target='_blank'>"+ ENTRIES[dataID].artist +"</a>"
+            modalHtml += 
+            // ENTRIES[dataID].ocName +
+            "By <a aref='"+ ENTRIES[dataID].artist+" target='_blank'>"+ ENTRIES[dataID].artist +"</a>"
             modalHtml += "</div>";
             modalHtml += "</div>";
             $(".modal_content").html(modalHtml);
@@ -211,14 +217,65 @@ function setUpArtModalClickEvents() {
     setupCloseModalEvents();
 }
 
+//===================================//
+//=== Settingup Modal Close Event ===//
+//===================================//
+
 function setupCloseModalEvents(){
-    $(".modal_bg").click(function () {
+    $(".modal_bg, .close").click(function () {
         $(".modal").addClass("hide");
         //$(document.body).removeClass("noscroll");
         resetScrollBar();
+        $(".itemArt").css("cursor","auto");
         CURRENT_SUMMARY_LANG = 0;
         CURRENT_ALT_INDEX = 0;
     })
+}
+
+
+//=============================//
+//=== Handle traslate Event ===//
+//=============================//
+
+// function setUpTraslateToggle(entry) {
+//     var newSummary;
+//     if(entry.giftDescriptionAlt.length==0){
+//         $(".langSwitch").css("display", "none");
+//     }
+//     $(".langSwitch").click(function () {
+//         if (CURRENT_SUMMARY_LANG === 0 && entry.giftDescriptionAlt.length>0) {
+//             newSummary = entry.giftDescriptionAlt;
+//             CURRENT_SUMMARY_LANG = 1
+//         } else {
+//             newSummary = entry.giftDescription;
+//             CURRENT_SUMMARY_LANG = 0;
+//         }
+//         $(".itemSummary_inner").html(newSummary);
+//     });
+// }
+
+//=============//
+//=== Other ===//
+//=============//
+
+function hideScrollBar(){
+    $('body').width($('body').width());
+    $('body').css('overflow', 'hidden');
+    $('.modal').css('display', 'block');
+    var windowWidth = $(window).width();
+    if (windowWidth >= 768) {
+        $('.decor_R').css('right','10px');
+    }
+
+    
+}
+function resetScrollBar(){
+    $('body, .modal').removeAttr('style')
+    var windowWidth = $(window).width();
+    if (windowWidth >= 768) {
+        $('.decor_R').css('right','0px');
+    }
+
 }
 
 function setupStuff() {
@@ -228,16 +285,6 @@ function setupStuff() {
     setUpItemModalClickEvents();
 }
 
-function hideScrollBar(){
-    $('body').width($('body').width());
-    $('body').css('overflow', 'hidden');
-    $('.modal').css('display', 'block');
-    $('.decor_R').css('right','10px');
-}
-function resetScrollBar(){
-    $('body, .modal').removeAttr('style')
-    $('.decor_R').css('right','0px');
-}
 //======================//
 //===                ===//
 //=== Ready Function ===//
