fecd7fd189b8840340705ddf4e92308b16ee46ca
haze111nuts <hasami75@gmail.com>
2024-10-13 19:49:08 -0400

    fill nav info for form

--- scripts/form.js
+++ scripts/form.js
@@ -6,23 +6,58 @@ function setUpConfirmEvent() {
 }
 
 function setUpNavClickEvents() {
-    $('.navlist li:nth-child(1)').click(function () {
-        $('.readme').toggleClass("hiddenContent");
-        $('.guide').addClass("hiddenContent");
-        // unhideCurrentNavItem('.readme')
-        // hideOtherNavItem('.readme');
-        $(this).siblings().removeClass("highlighted");
-        $(this).toggleClass("highlighted");
+    $(".navlist li").each(function () {
+        $(this).on("click", function () {
+            $('.' + $(this).attr("for")).toggleClass("hiddenContent");
+            $('.' + $(this).attr("for")).siblings().addClass("hiddenContent");
+            $(this).siblings().removeClass("highlighted");
+            $(this).toggleClass("highlighted");
+        })
     });
+}
 
-    $('.navlist li:nth-child(2)').click(function () {
-        $('.guide').toggleClass("hiddenContent");
-        $('.readme').addClass("hiddenContent");
-        // unhideCurrentNavItem('.guide')
-        // hideOtherNavItem('.guide');
-        $(this).siblings().removeClass("highlighted");
-        $(this).toggleClass("highlighted");
-    });
+function setUpTimer() {
+    //check which deadline is the closest one
+    //and decide the index
+    var index = 1;
+
+    //set deadline name
+    $(".countdown_label span").html($(".deadlines ul li:nth-child("+index+") span:nth-child(1)").html());
+
+    //highlight the current deadline
+    var deadline = $(".deadlines ul li:nth-child("+index+")").addClass("currentDeadline");
+
+    //grab deadline
+    var deadline = $(".deadlines ul li:nth-child("+index+") span:nth-child(2)").html();
+    var compareDate = new Date(deadline);
+    compareDate.setDate(compareDate.getDate());
+
+    setInterval(function () {
+        setTimeBetweenDates(compareDate);
+    }, 500);
+}
+
+function setTimeBetweenDates(toDate) {
+    var dateEntered = toDate;
+    var now = new Date();
+    var difference = dateEntered.getTime() - now.getTime();
+
+    if (difference <= 0) {
+        // Timer done
+        clearInterval(timer);
+    } else {
+        var seconds = Math.floor(difference / 1000);
+        var minutes = Math.floor(seconds / 60);
+        var hours = Math.floor(minutes / 60);
+        var days = Math.floor(hours / 24);
+        hours %= 24;
+        minutes %= 60;
+        seconds %= 60;
+        $(".days").text(days);
+        $(".hours").text(hours);
+        $(".minutes").text(minutes);
+        $(".seconds").text(seconds);
+    }
 }
 
 //======================//
@@ -34,4 +69,6 @@ function setUpNavClickEvents() {
 $(document).ready(function () {
     setUpConfirmEvent();
     setUpNavClickEvents();
+    setUpTimer();
+
 });
