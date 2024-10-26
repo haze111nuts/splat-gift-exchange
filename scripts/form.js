b2805a3fd17bc0ed92576b70ebf5f358e6f06c34
chsiao58 <chsiao58@gmail.com>
2024-10-26 12:22:47 -0400

    lang

--- scripts/form.js
+++ scripts/form.js
@@ -832,6 +832,9 @@ function setupSampleGiftModalHtml(entries) {
 //======================//
 
 $(document).ready(function () {
+    var language = window.navigator.userLanguage || window.navigator.language;
+
+    IS_ENG_FORM = language === "en-US" ? true : false;
     setUpTimer();
     setUpConfirmEvent();
     setUpNavClickEvents();
